b85b23b576498860ee527fc2cf5891283d1f1d21
Anton Bagly <taccessviolation@gmail.com>
2023-06-20 20:16:39 +0300

    WIP: configs, services and install

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,6 +4,11 @@
 # COMMON VARIABLES
 #=================================================
 
+# dependencies used by the app
+pkg_dependencies="redis-server"
+
+nodejs_version=16
+
 #=================================================
 # PERSONAL HELPERS
 #=================================================
